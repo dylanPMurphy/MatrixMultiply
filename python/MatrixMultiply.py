0bae7b2fbc648fe72e3349de692b969cc3a7d1f3
Dylan Murphy <dylanmurphydba@gmail.com>
2021-09-28 23:54:40 -0700

    matrix class started

---
+++ python/MatrixMultiply.py
@@ -0,0 +1,30 @@
+# #implement a function that multiplies two matricies
+# def matrixMultiply-areCompatible(m1, m2)
+#   m1-row-length = m1.length
+#   m2-row-length = m2.length
+#   m1-column-length = m1[0].length
+#   m2-column-length = m2[0].length
+
+# def matrxMultiply(m1, m2):
+
+#   #Calculate matrix dimensions
+
+class Matrx:
+	def __init__(self, data):
+		self.data = value
+
+    @property
+    def row_length(self):
+        return self.data.length
+    @property
+    def column_length(self):
+        return self.data[0].length
+
+    def multiplyCompatable(self, matrix):
+        if this.column_length == matrix.row_length:
+            return true
+        else:
+            return false
+
+    def multiply(self, matrix):
+        pass
